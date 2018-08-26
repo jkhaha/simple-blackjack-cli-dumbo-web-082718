f9893d373d7737d8253b2f23a37d6512f305a344
Jackie Ha <jha2@wellesley.edu>
2018-08-26 16:06:59 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -64,9 +64,8 @@ def runner
   welcome
   hand = initial_round
   until card_total > 21
-  card_total = hit?(card_total)
-  card_total = display_card_total(card_total)
+  hand = hit?(hand)
+  display_card_total(hand)
 end
-end_game(card_total)
-end
+end_game(hand)
 end
